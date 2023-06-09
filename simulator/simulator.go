fdee73b01b25f286b9f02da74806091920261499
Tauchid <muhammadtauchid@gmail.com>
2023-06-09 14:29:15 +0700

    add Simulator module

---
+++ simulator/simulator.go
@@ -0,0 +1,16 @@
+package simulator
+
+import (
+    "os"
+    "time"
+)
+
+type Simulator interface {
+    Get(Trace) error
+    PrintToFile(file *os.File, start time.Time) error
+}
+
+type Trace struct {
+    Addr    int
+    Op      string
+}
